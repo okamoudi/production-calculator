84a9e81dd636af6821601bc3aab7e65a7a4a4e49
Omar Alamoudi <info@omaralamoudi.net>
2025-10-16 16:50:12 +0300

    Refine the look and add some featurs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const productionDateInput = document.getElementById('production-date');
-const pagesPerEpisodeInput = document.getElementById('pages-per-episode'); // New field
+const pagesInput = document.getElementById('pages'); // New field
 const pagesPerDayInput = document.getElementById('pages-per-day'); // New field
 const episodeCountInput = document.getElementById('episodes-count'); // New field
 const workingDaysWeekInput = document.getElementById('working-days-week'); // New field
@@ -15,6 +15,11 @@ const productionWeeksResult = document.getElementById('production-weeks-result')
 const productionMonthsResult = document.getElementById('production-months-result');
 const productionMonthsWeeksDaysResult = document.getElementById('production-months-weeks-days-result');
 const daysResult = document.getElementById('days-to-production'); // Make this write-only
+const filmProjectOptionInput = document.getElementById('film-project');
+const seriesProjectOptionInput = document.getElementById('series-project');
+const episodeCountDiv = document.getElementById('episodes-count-div'); // New field
+const pagesLable = document.getElementById('pages-label'); // New field
+var lastEnteredEpisodeNumber = 1;
 
 const picker = new Pikaday({
   field: productionDateInput,
@@ -25,14 +30,13 @@ const picker = new Pikaday({
   minDate: new Date() // Set minimum date to today
 });
 
-
 function calculateData() {
   const today = new Date();
   const productionDate = new Date(productionDateInput.value);
-  const pagesPerEpisode = parseInt(pagesPerEpisodeInput.value) || 0; // Handle potential empty input
+  const pages = parseInt(pagesInput.value) || 1; // Handle potential empty input
   const pagesPerDay = parseFloat(pagesPerDayInput.value) || 1; // Handle potential empty input
   const episodes_count = parseInt(episodeCountInput.value) || 1; // Handle potential empty input
-  const workingDaysWeek = parseInt(workingDaysWeekInput.value) || 1; // Handle potential empty input
+  const workingDaysWeek = parseInt(workingDaysWeekInput.value) || 6; // Handle potential empty input
 
   // Check if production date is after today
   if (productionDate < today) {
@@ -49,7 +53,7 @@ function calculateData() {
   }
     // Calculate estimated days based on pages (replace with your actual logic)
   
-    const daysPerEpisode = (pagesPerEpisode / pagesPerDay);
+    const daysPerEpisode = (pages / pagesPerDay);
     const estimatedDays = Math.ceil( daysPerEpisode * episodes_count) || 0; /* Your logic for calculating days per page */
   // Combine estimated days with user-provided days (optional)
     
@@ -78,10 +82,10 @@ function calculateData() {
   // Calculate end date based on production days and weeks
   const endDate = new Date(productionDate.getTime() + (productionCalendarDays) * 24 * 60 * 60 * 1000);
     
-    const weeksReminder = Math.floor((days % 30) / 7) ;
-    const daysReminder = Math.ceil((days % 30) % 7);
-    const prodWeeksReminder = Math.floor((productionCalendarDays % 30) / 7) ;
-    const prodDaysReminder = Math.ceil((productionCalendarDays % 30)% 7);
+  const weeksReminder = Math.floor((days % 30) / 7) ;
+  const daysReminder = Math.ceil((days % 30) % 7);
+  const prodWeeksReminder = Math.floor((productionCalendarDays % 30) / 7) ;
+  const prodDaysReminder = Math.ceil((productionCalendarDays % 30)% 7);
     
   // Format end date in YYYY-MM-DD
   const formattedStartDate = productionDate.toISOString().slice(0, 10);
@@ -106,26 +110,80 @@ function calculateData() {
 
 
     }
-
   // Store values in session storage
   sessionStorage.setItem('productionDate', productionDateInput.value);
-  sessionStorage.setItem('pagesPerEpisode', pagesPerEpisode);
+  sessionStorage.setItem('pages', pages);
   sessionStorage.setItem('pagesPerDay', pagesPerDay);
   sessionStorage.setItem('episodeCount', episodeCountInput.value);
 }
-
-// Retrieve values from session storage on page load
-// and populate input fields if available
-const storedProductionDate = sessionStorage.getItem('productionDate');
-const storedPagesPerEpisode = sessionStorage.getItem('pagesPerEpisode');
-const storedPagesPerDay = sessionStorage.getItem('pagesPerDay');
-const storedEpisodeCount = sessionStorage.getItem('episodeCount');
-
-if (storedProductionDate && storedPagesPerEpisode && storedPagesPerDay && storedEpisodeCount) {
-  productionDateInput.value = storedProductionDate;
-  pagesPerEpisodeInput.value = storedPagesPerEpisode;
-  pagesPerDayInput.value = storedPagesPerDay;
-  episodeCountInput.value = storedEpisodeCount;
-  // Recalculate data based on retrieved values (optional)
+function episodeCountReset(){
+  const selectedProjectType = document.querySelector('input[name="project-type"]:checked');
+  if (selectedProjectType.value != 'film'){
+      lastEnteredEpisodeNumber = parseInt(episodeCountInput.value);
+      sessionStorage.setItem('lastEnteredEpisodeNumber', lastEnteredEpisodeNumber);
+  }
   calculateData();
 }
+function projectTypeSelection(){
+  const selectedProjectType = document.querySelector('input[name="project-type"]:checked');
+  if (selectedProjectType.value == "film"){
+    episodeCountInput.value = 1;
+    episodeCountDiv.hidden = true;
+    pagesLable.innerHTML = "Script Length:";
+  } else {
+    episodeCountDiv.hidden = false;
+    pagesLable.innerHTML = "Average Episode Script Length:";
+    episodeCountInput.value = lastEnteredEpisodeNumber;
+  }
+  saveProjectTypeSelection(selectedProjectType);
+  calculateData();
+}
+function saveProjectTypeSelection(projectType) {
+  if (projectType) {
+    sessionStorage.setItem('selectedProjectTypeChoice', projectType.value);
+    sessionStorage.setItem('episodeCountDivHidden',episodeCountDiv.hidden);
+  }
+}
+
+function loadProjectTypeSelection() {
+  // Retrieve values from session storage on page load
+  // and populate input fields if available
+  const storedProductionDate = sessionStorage.getItem('productionDate');
+  const storedpages = sessionStorage.getItem('pages');
+  const storedPagesPerDay = sessionStorage.getItem('pagesPerDay');
+  const storedEpisodeCount = sessionStorage.getItem('episodeCount');
+  const storedProjectType = sessionStorage.getItem('selectedProjectTypeChoice');
+  const storedEpisodeCountDivHidden = sessionStorage.getItem('episodeCountDivHidden');
+  const storedLastEnteredEpisodeNumber = sessionStorage.getItem('lastEnteredEpisodeNumber');
+  
+  if (storedProductionDate) {
+    productionDateInput.value = storedProductionDate;
+  }
+  if (storedpages){
+    pagesInput.value = storedpages;
+  }
+  if(storedPagesPerDay){
+    pagesPerDayInput.value = storedPagesPerDay;
+  }
+  if (storedEpisodeCount){
+    episodeCountInput.value = storedEpisodeCount;
+  }
+  if (storedProjectType) {
+    const projectTypToSelect = document.querySelector(`input[name="project-type"][value="${storedProjectType}"]`);
+    if (projectTypToSelect) {
+      projectTypToSelect.checked = true;
+      if (projectTypToSelect.value == "film") {
+      episodeCountDiv.hidden = true;
+      }
+    }
+  }
+  if (storedLastEnteredEpisodeNumber){
+    lastEnteredEpisodeNumber = storedLastEnteredEpisodeNumber;
+  }
+  // Recalculate data based on retrieved values (optional)
+  if (storedProductionDate && storedpages && storedPagesPerDay && storedEpisodeCount){
+    calculateData();
+  }
+}
+loadProjectTypeSelection();
+
